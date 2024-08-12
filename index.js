54438e6d4cb7c6d9cae87348c315ef58aac0b000
abu zayd elkhawarizmy <69622597+abuzaydelkhwarizmy@users.noreply.github.com>
2024-08-12 13:36:03 +0700

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ dayjs.extend(timezone);
 
 dotenv.config();
 
-// make sure all env variables are set
+// Make sure all env variables are set
 invariant(process.env.GEO_LATITUDE, "secret GEO_LATITUDE is required");
 invariant(process.env.GEO_LONGITUDE, "secret GEO_LONGITUDE is required");
 invariant(process.env.ACCOUNT_EMAIL, "secret ACCOUNT_EMAIL is required");
@@ -39,7 +39,7 @@ const PUBLIC_HOLIDAYS = [
   "26 Dec 2023", // cuti bersama natal
 ];
 
-async function mainLogic() {
+const mainLogic = async () => {
   const isHeadless =
     (process.env.HEADLESS_BROWSER ?? "true") === "true" ? true : false;
 
@@ -65,142 +65,132 @@ async function mainLogic() {
 
   const page = await context.newPage();
 
-  console.log("Opening login page...");
-  await page.goto(
-    "https://account.mekari.com/users/sign_in?client_id=TAL-73645&return_to=L2F1dGg_Y2xpZW50X2lkPVRBTC03MzY0NSZyZXNwb25zZV90eXBlPWNvZGUmc2NvcGU9c3NvOnByb2ZpbGU%3D",
-    { timeout: 60000 } // Set timeout to 60 seconds
-  );
+  try {
+    console.log("Opening login page...");
+    await page.goto(
+      "https://account.mekari.com/users/sign_in?client_id=TAL-73645&return_to=L2F1dGg_Y2xpZW50X2lkPVRBTC03MzY0NSZyZXNwb25zZV90eXBlPWNvZGUmc2NvcGU9c3NvOnByb2ZpbGU%3D"
+    );
 
-  await page.setViewportSize({ width: 1080, height: 560 });
+    // Take a screenshot for debugging
+    await page.screenshot({ path: 'before_email_input.png' });
 
-  console.log("Filling in account email & password...");
-  await page.waitForSelector("#user_email", { timeout: 60000 });
-  await page.click("#user_email");
-  await page.fill("#user_email", process.env.ACCOUNT_EMAIL);
+    console.log("Filling in account email & password...");
+    await page.waitForSelector("#user_email", { timeout: 90000 }); // Increased timeout to 90s
+    await page.click("#user_email");
+    await page.fill("#user_email", process.env.ACCOUNT_EMAIL);
 
-  await page.press("#user_email", "Tab");
-  await page.fill("#user_password", process.env.ACCOUNT_PASSWORD);
+    await page.press("#user_email", "Tab");
+    await page.fill("#user_password", process.env.ACCOUNT_PASSWORD);
 
-  console.log("Signing in...");
-  await page.click("#new-signin-button");
-  await page.waitForSelector('a[href="/employee/dashboard"]', { timeout: 60000 }); // wait up to 60 seconds
+    console.log("Signing in...");
+    await page.click("#new-signin-button");
+    await page.waitForSelector('a[href="/employee/dashboard"]', { timeout: 90000 }); // wait up to 90 seconds   
 
-  const dashboardNav = page.getByText("Dashboard");
-  // check if dashboard nav is exist
-  if ((await dashboardNav.innerText()) === "Dashboard") {
-    console.log("Successfully Logged in...");
-  }
-
-  const myName = (await page.locator("#navbar-name").textContent()).trim();
-
-  async function isOffToday(page, myName) {
-    // Wait for the "Who's Off" section to load
-    await page.waitForSelector('.tl-card-small', { timeout: 60000 });
-  
-    // Extract the names of people who are off today
-    const offPeople = await page.$$eval('.tl-leave-list__item .font-weight-bold', elems => elems.map(e => e.innerText));
-  
-    // Check if the user is off today
-    return offPeople.includes(myName);
-  }
-
-  const isUserOffToday = await isOffToday(page, myName);
-  if (isUserOffToday) {
-    console.log("You are off today, skipping check in/out...");
-    await browser.close();
-    return;
-  }
-
-  // go to "My Attendance Logs"
-  await page.click("text=My Attendance Logs");
-  await page.waitForSelector('h1:text("My attendance log")', { timeout: 60000 });
-  console.log(
-    "Already inside My Attendance Logs to check holiday or day-off..."
-  );
-
-  const rowToday = page.locator("tr", { hasText: TODAY }).first();
-
-  const columnCheckDayOff = await rowToday
-    .locator("td:nth-child(2)")
-    .innerText();
-
-  const columnCheckOnLeave = await rowToday
-    .locator("td:nth-child(7)")
-    .innerText();
-
-  // N = not dayoff/holiday
-  const isTodayHoliday = columnCheckDayOff.trim() !== "N";
-
-  // CT = cuti
-  const isTodayOnLeave = columnCheckOnLeave.trim() === "CT";
-
-  const shouldSkipCheckInOut = isTodayHoliday || isTodayOnLeave;
-
-  if (shouldSkipCheckInOut) {
-    const consoleText = isTodayOnLeave
-      ? "You are on leave (cuti) today, skipping check in/out..."
-      : "You are on holiday today, skipping check in/out...";
-    console.log(consoleText);
-
-    await browser.close();
-    return;
-  }
-
-  await Promise.all([
-    page.goto("https://hr.talenta.co/live-attendance"),
-    page.waitForNavigation(),
-  ]);
-
-  console.log("Already inside Live Attendance Page...");
-
-  const currentTime = await page.waitForSelector(".current-time");
-  const checkIn = await page.waitForSelector(".col:nth-child(1) > .btn");
-  const checkOut = await page.waitForSelector(".col:nth-child(2) > .btn");
-
-  console.log("Current Time: ", await currentTime.innerText());
-  console.log("Found: ", await checkIn.innerText());
-  console.log("Found: ", await checkOut.innerText());
-
-  if (process.env.SKIP_CHECK_IN_OUT === "true") {
-    console.log("Skipping Check In/Out...");
-    await browser.close();
-    return;
-  }
-
-  if (process.env.CHECK_TYPE === "CHECK_IN") {
-    console.log("Checking In...");
-    await page.click(".col:nth-child(1) > .btn");
-  } else if (process.env.CHECK_TYPE === "CHECK_OUT") {
-    console.log("Checking Out...");
-    await page.click(".col:nth-child(2) > .btn");
-  }
-
-  if (process.env.CHECK_TYPE === "CHECK_IN") {
-    const toast = page.getByText("Successfully Clock In");
-    if ((await toast.innerText()) === "Successfully Clock In") {
-      console.log("Successfully Clock In");
+    const dashboardNav = page.getByText("Dashboard");
+    if ((await dashboardNav.innerText()) === "Dashboard") {
+      console.log("Successfully Logged in...");
     }
-  } else if (process.env.CHECK_TYPE === "CHECK_OUT") {
-    const toast = page.getByText("Successfully Clock Out");
-    if ((await toast.innerText()) === "Successfully Clock Out") {
-      console.log("Successfully Clock Out");
-    }
-  }
 
-  await browser.close();
-}
+    const myName = (await page.locator("#navbar-name").textContent()).trim();
+
+    async function isOffToday(page, myName) {
+      await page.waitForSelector('.tl-card-small', { timeout: 90000 });
+      const offPeople = await page.$$eval('.tl-leave-list__item .font-weight-bold', elems => elems.map(e => e.innerText));
+      return offPeople.includes(myName);
+    }
+
+    const isUserOffToday = await isOffToday(page, myName);
+    if (isUserOffToday) {
+      console.log("You are off today, skipping check in/out...");
+      await browser.close();
+      return;
+    }
+
+    await page.click("text=My Attendance Logs");
+    await page.waitForSelector('h1:text("My attendance log")', { timeout: 90000 });
+    console.log("Already inside My Attendance Logs to check holiday or day-off...");
+
+    const rowToday = page.locator("tr", { hasText: TODAY }).first();
+
+    const columnCheckDayOff = await rowToday.locator("td:nth-child(2)").innerText();
+    const columnCheckOnLeave = await rowToday.locator("td:nth-child(7)").innerText();
+
+    const isTodayHoliday = columnCheckDayOff.trim() !== "N";
+    const isTodayOnLeave = columnCheckOnLeave.trim() === "CT";
+
+    const shouldSkipCheckInOut = isTodayHoliday || isTodayOnLeave;
+
+    if (shouldSkipCheckInOut) {
+      const consoleText = isTodayOnLeave
+        ? "You are on leave (cuti) today, skipping check in/out..."
+        : "You are on holiday today, skipping check in/out...";
+      console.log(consoleText);
+
+      await browser.close();
+      return;
+    }
+
+    await Promise.all([
+      page.goto("https://hr.talenta.co/live-attendance"),
+      page.waitForNavigation(),
+    ]);
+
+    console.log("Already inside Live Attendance Page...");
+
+    const currentTime = await page.waitForSelector(".current-time");
+    const checkIn = await page.waitForSelector(".col:nth-child(1) > .btn");
+    const checkOut = await page.waitForSelector(".col:nth-child(2) > .btn");
+
+    console.log("Current Time: ", await currentTime.innerText());
+    console.log("Found: ", await checkIn.innerText());
+    console.log("Found: ", await checkOut.innerText());
+
+    if (process.env.SKIP_CHECK_IN_OUT === "true") {
+      console.log("Skipping Check In/Out...");
+      await browser.close();
+      return;
+    }
+
+    if (process.env.CHECK_TYPE === "CHECK_IN") {
+      console.log("Checking In...");
+      await page.click(".col:nth-child(1) > .btn");
+    } else if (process.env.CHECK_TYPE === "CHECK_OUT") {
+      console.log("Checking Out...");
+      await page.click(".col:nth-child(2) > .btn");
+    }
+
+    if (process.env.CHECK_TYPE === "CHECK_IN") {
+      const toast = page.getByText("Successfully Clock In");
+      if ((await toast.innerText()) === "Successfully Clock In") {
+        console.log("Successfully Clock In");
+      }
+    } else if (process.env.CHECK_TYPE === "CHECK_OUT") {
+      const toast = page.getByText("Successfully Clock Out");
+      if ((await toast.innerText()) === "Successfully Clock Out") {
+        console.log("Successfully Clock Out");
+      }
+    }
+  } catch (error) {
+    console.error("An error occurred:", error);
+    await page.screenshot({ path: 'error_screenshot.png' });
+    throw error;
+  } finally {
+    await browser.close();
+  }
+};
 
 const MAX_RETRIES = 3;
 
 for (let attempt = 1; attempt <= MAX_RETRIES; attempt++) {
-    try {
-        await mainLogic();
-        break;  // exit the loop if successful
-    } catch (error) {
-        if (error.name === 'TimeoutError' && attempt < MAX_RETRIES) {
-            console.log(`Attempt ${attempt} failed due to a timeout. Retrying...`);
-        } else {
-            throw error;  // re-throw the error if it's not a TimeoutError or if we've reached the max retries
-        }
+  try {
+    await mainLogic();
+    break; // exit the loop if successful
+  } catch (error) {
+    if (error.name === 'TimeoutError' && attempt < MAX_RETRIES) {
+      console.log(`Attempt ${attempt} failed due to a timeout. Retrying...`);
+    } else {
+      console.error("Final attempt failed:", error);
+      throw error; // re-throw the error if it's not a TimeoutError or if we've reached the max retries
     }
+  }
 }
